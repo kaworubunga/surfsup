b96d2cc644516ea6b3f6e734327db530b5941897
kaworubunga <surf@surfsup.dev>
2022-09-12 20:52:07 -0700

    forgot to buld xdd

--- src/components/slideShow.tsx
+++ src/components/slideShow.tsx
@@ -5,7 +5,6 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-import website from '../assets/images/website.png'
 import images from '../assets/images/images'
 
 const SlideShowWrapper = styled.div`
